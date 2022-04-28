3e0c6039dfaaedc48143cb34a6dd03fec56a08f8
Pallagi Joe <pallagijoe@gmail.com>
2022-04-28 09:01:05 +0200

    code format

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -5,6 +5,7 @@ function ExpenseItem(props) {
     const month = props.date.toLocaleString('en-US', {month : 'long'})
     const day = props.date.toLocaleString('en-US', {day : '2-digit'})
     const year = props.date.getFullYear();
+    
     return (
         <div className='expense-item'>
             <div>
